cba7d4ec55bd0ab0869985c4e7a9711de6c9bca8
Himanshu Singh <himanshu.sap124@gmail.com>
2026-07-21 09:28:04 +0000

    consume api

--- srv/custom-service.js
+++ srv/custom-service.js
@@ -1,50 +1,52 @@
 import cds from "@sap/cds";
-import SdkConnectivity from '@sap-cloud-sdk/connectivity';
-import SdkHttpClient from '@sap-cloud-sdk/http-client';
+import axios from 'axios';
+import qs from 'qs';
 
 const CustomService = async (srv) => {
 
     srv.on('getCustomData', async (request) => {
         try {
-            // Read Destination and endpoint details maintained on User Provided Variables
-            const destination = process.env.LOG_DESTINAITION_NAME ?? null ;
-            const endpoint = process.env.LOG_DESTINAITION_ENDPOINT ?? null ;
 
-            if(!destination || !endpoint) {
-                return request.error({ code: 400, message: 'Destination or Endpoint not configured' });
-            }
+            // Replace <region> with your actual region in the baseUrl
+            const baseUrl = 'https://auditlog-management.cfapps.<region>.hana.ondemand.com';
 
-            const myDestination = await SdkConnectivity.getDestination({
-                destinationName: destination
-            });
+            
+            const clientId = 'sb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxb6316';
+            const clientSecret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
-            if (!myDestination) {
-                return request.error({ code: 404, message: 'Destination not found' });
-            }
+            const tokenURL = 'https://<subdomain>.authentication.<region>.hana.ondemand.com/oauth/token';
 
+            const endpoint = '/auditlog/v2/auditlogrecords?time_from=2026-07-10T00:00:00&time_to=2026-07-10T00:03:00';
 
-            console.log('Destination retrieved:', myDestination.name);
-
-            const destinationResponse = await SdkHttpClient.executeHttpRequest(myDestination, {
-                    url: endpoint,
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                    // data: JSON.stringify(requestBody)
+            // Axios POST call to Generate Token
+            const tokenResponse = await axios.post(tokenURL, qs.stringify(
+                {
+                    grant_type: 'client_credentials'
                 }
-            );
-
-            if (!destinationResponse) {
-                return request.error({ code: 404, message: 'No response from Destination' });
-            }
-
-            const data = destinationResponse.data;
+            ), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
+                }
+            });
+            
+            
+            const accessToken = await tokenResponse.data.access_token;
+            
+            const apiUri = baseUrl + endpoint;
+            
+            // Axios GET call to fetch API data
+            const apiResponse = await axios.get(apiUri, {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Accept': 'application/json'
+                }
+            });
 
 
             return {
                 status: 200,
-                data: { message: destinationResponse.data }
+                data: { message: apiResponse.data }
             }
         }
         catch (error) {
